874127894dc9de856c353763e92901a3cfe56fff
Kia Farhang <kfarhang0@gmail.com>
2017-09-30 14:56:35 -0500

    Moved App component inside the components folder

--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-global.requestAnimationFrame = (cb) => {
-    setTimeout(cb, 0)
-}
-
-Enzyme.configure({ adapter: new Adapter() });
