d32d9019737772cd32b78544299bc4e66fc3841f
Kia Farhang <kfarhang0@gmail.com>
2017-09-30 14:51:20 -0500

    Wrote basic Text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export interface Props {
+    text: string;
+}
+
+const Text = ({ text }: Props) => {
+    return (
+        <p>{text}</p>
+    )
+}
+
+export default Text;
