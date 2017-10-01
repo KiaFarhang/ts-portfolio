d8d395c1bb04c7f3c7a7faed5dfa0ab2dc5db825
Kia Farhang <kfarhang0@gmail.com>
2017-10-01 17:19:30 -0500

    Changed App to a stateless functional component, switched it to Router with routes

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,30 +1,33 @@
 import * as React from 'react';
 import {
   BrowserRouter as Router,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
+
+import HomePage from '../HomePage';
 
 import './App.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        </div>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Route exact={true} path="/" component={HomePage} />
+
+    </Router>
+  );
+};
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <Router>
+//         <Route exact path="/" component={HomePage} />
+
+//       </Router>
+//     );
+//   }
+// }
 
 export default App;
