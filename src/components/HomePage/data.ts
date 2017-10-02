94ff4c81c09b435b03a0bb4e3b09a52371fa6e1d
Kia Farhang <kfarhang0@gmail.com>
2017-10-01 19:57:11 -0500

    Working on text for home page

--- src/components/HomePage/data.ts
+++ src/components/HomePage/data.ts
@@ -1,6 +1,10 @@
 const data = {
     text: [
-        `Hi, I'm Kia Farhang.`
+        `Hi, I'm Kia Farhang. I'm a full-stack developer at the Express-News in San Antonio, Texas.`,
+        `This website showcases my work and a little bit more about myself.`,
+        `You can navigate using the links below - or, if you'd like, use the command line at the bottom of the screen.`,
+        `Simply type the name of the page you'd like to visit. 
+        For example, to access the "About" page, type "about" or "About" and press enter.`
     ],
     links: [
         { text: 'About', href: 'about' }
