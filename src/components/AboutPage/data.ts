d47d420be3cd0253991bbe74a9f0a2fc156d28a5
Kia Farhang <kfarhang0@gmail.com>
2017-10-07 11:13:23 -0500

    Wrote about page content

--- src/components/AboutPage/data.ts
+++ src/components/AboutPage/data.ts
@@ -1,6 +1,9 @@
 const data = {
     text: [
-        `This is the about page`
+        `I grew up in Minnesota, where I studied journalism in college.`,
+        `That took me to Southern California, where I started coding and eventually switched careers.`,
+        `I prefer back-end, test-driven development whenever possible. In spring 2018, I'll begin the part-time pursuit of a computer science degree.`,
+        `Outside of work, I enjoy playing video games, reading and taking walks with my girlfriend and dog.`
     ],
     links: [
         { text: 'Home', href: '' }
