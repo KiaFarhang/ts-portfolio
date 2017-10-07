88a35fb515d19cebe7949d92ae1174d21376af4b
Kia Farhang <kfarhang0@gmail.com>
2017-10-07 14:44:57 -0500

    Created separate Projects folder under Portfolio page to keep data on subpages

---
+++ src/components/PortfolioPage/Projects/tceq.ts
@@ -0,0 +1,18 @@
+const tceq = {
+    text: [
+        `The Texas Commission on Environmental Quality receives dozens of complaints every week regarding possible violations of environmental law.`,
+        `This scraper manipulates the group's ASP.NET web form daily, adding each complaint to a database and emailing it to the environmental reporter at the Express-News.`,
+        `Use the "repo" command to read more on GitHub.`
+    ],
+    links: [
+        { text: 'portfolio', href: 'portfolio' },
+        { text: 'repo', href: 'https://github.com/sa-express-news/tceq-scraper' }
+
+    ],
+    commands: [
+        { command: 'portfolio', url: 'portfolio' },
+        { command: 'repo', url: 'https://github.com/sa-express-news/tceq-scraper' }
+    ]
+};
+
+export default tceq;
