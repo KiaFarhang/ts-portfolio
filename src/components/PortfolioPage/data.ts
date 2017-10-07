6a7697f988213e766290091877b7073df7e9d7a7
Kia Farhang <kfarhang0@gmail.com>
2017-10-07 14:46:12 -0500

    Built out portfolio page

--- src/components/PortfolioPage/data.ts
+++ src/components/PortfolioPage/data.ts
@@ -1,15 +1,24 @@
 const data = {
     text: [
-        `This is portfolio`
+        `Here are a few of my recent projects.`,
+        `You can also use the "github" command to open my GitHub profile.`
     ],
     links: [
-        { text: 'Home', href: '' },
-        { text: 'About', href: 'about' }
+        { text: 'home', href: '' },
+        { text: 'about', href: 'about' },
+        { text: 'github', href: 'https://github.com/kiafarhang' },
+        { text: 'story-template', href: 'portfolio/story-template' },
+        { text: 'tceq-scraper', href: 'portfolio/tceq' },
+        { text: 'oubliette', href: 'portfolio/oubliette' }
 
     ],
     commands: [
         { command: 'home', url: '' },
-        { command: 'about', url: 'about' }
+        { command: 'about', url: 'about' },
+        { command: 'github', url: 'https://github.com/kiafarhang' },
+        { command: 'story-template', url: 'portfolio/story-template' },
+        { command: 'tceq-scraper', url: 'portfolio/tceq' },
+        { command: 'oubliette', url: 'portfolio/oubliette' }
     ]
 };
 
--- src/components/PortfolioPage/index.tsx
+++ src/components/PortfolioPage/index.tsx
@@ -4,9 +4,17 @@ import Page from '../Page';
 
 import data from './data';
 
-const PortfolioPage = () => {
+import tceq from './Projects/tceq';
+import AboutPage from '../AboutPage';
+
+import { RouteComponentProps, Route, Router } from 'react-router-dom';
+
+const PortfolioPage = ({ match, location, history }: RouteComponentProps<{}>) => {
     return (
-        <Page text={data.text} links={data.links} commandMatches={data.commands} />
+        <div>
+            <Route exact={true} path={match.url} render={() => <Page text={data.text} links={data.links} commandMatches={data.commands} />} />
+            <Route path={`${match.url}/tceq`} render={() => <Page text={tceq.text} links={tceq.links} commandMatches={tceq.commands} />} />
+        </div>
     );
 };
 
