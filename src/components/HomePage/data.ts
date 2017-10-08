aefbb6550007bdbafcda1de3c5cae64bb02f143a
Kia Farhang <kfarhang0@gmail.com>
2017-10-08 11:46:40 -0500

    Tweaked home page wording

--- src/components/HomePage/data.ts
+++ src/components/HomePage/data.ts
@@ -1,10 +1,11 @@
 const data = {
     text: [
-        `Hi, I'm Kia Farhang. I'm a full-stack developer at the Express-News in San Antonio, Texas.`,
+        `Hi, I'm Kia Farhang, a full-stack developer at the Express-News in San Antonio, Texas.`,
         `This website showcases my work and a little bit about myself.`,
         `You can navigate using the links below - or, if you'd like, use the command line at the bottom of the screen.`,
-        `Simply type the name of the page you'd like to visit. 
-        For example, to access the "About" page, type "about" or "About" and press enter.`
+        `Type the name of the page you'd like to visit. 
+        To access the "about" page, type "about" and press enter.`,
+        `"../" will take you up one level.`
     ],
     links: [
         { text: 'about', href: 'about' },
