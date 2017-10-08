322576494e714941ecdd8365d7d5b6829351efd1
Kia Farhang <kfarhang0@gmail.com>
2017-10-08 10:42:56 -0500

    Added a link to a story that uses the template

--- src/components/PortfolioPage/Projects/template.ts
+++ src/components/PortfolioPage/Projects/template.ts
@@ -1,18 +1,20 @@
 const template = {
     text: [
-        `When I joined the Express-News, we had to use an expensive third-party tool to dress up important long-form journalism.`,
+        `When I joined the Express-News, we had to use an expensive third-party tool to dress up big stories on the web.`,
         `I used React and ArchieML, a markup language created at the New York Times, to build a longform template in house.`,
-        `Use the "repo" command to read more on GitHub.`
+        `Use the "repo" command to read more on GitHub. Use "example" to see a story that uses the template.`
     ],
     links: [
         { text: 'portfolio', href: '../portfolio' },
-        { text: 'repo', href: 'https://github.com/sa-express-news/longform-story-template' }
+        { text: 'repo', href: 'https://github.com/sa-express-news/longform-story-template' },
+        { text: 'example', href: 'http://www.expressnews.com/40-days-of-mourning/' }
 
     ],
     commands: [
         { command: '../', url: '../portfolio' },
         { command: 'portfolio', url: '../portfolio' },
-        { command: 'repo', url: 'https://github.com/sa-express-news/longform-story-template' }
+        { command: 'repo', url: 'https://github.com/sa-express-news/longform-story-template' },
+        { command: 'example', url: 'http://www.expressnews.com/40-days-of-mourning/' }
     ]
 };
 
