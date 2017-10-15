ca9c4c8192ec0fe0732986e5f435fed81fb93ca8
Kia Farhang <kfarhang0@gmail.com>
2017-10-15 11:16:50 -0500

    Added links to resume to home, about pages

--- src/components/AboutPage/data.ts
+++ src/components/AboutPage/data.ts
@@ -9,6 +9,7 @@ const data = {
     links: [
         { text: 'home', href: '' },
         { text: 'portfolio', href: 'portfolio' },
+        { text: 'resume', href: 'https://github.com/KiaFarhang/ts-portfolio/blob/master/kia-farhang-resume.pdf' },
         { text: 'linkedin', href: 'https://www.linkedin.com/in/kia-farhang-68423966/' }
 
     ],
@@ -16,6 +17,7 @@ const data = {
         { command: '../', url: '../' },
         { command: 'home', url: '' },
         { command: 'portfolio', url: 'portfolio' },
+        { command: 'resume', url: 'https://github.com/KiaFarhang/ts-portfolio/blob/master/kia-farhang-resume.pdf' },
         { command: 'linkedin', url: 'https://www.linkedin.com/in/kia-farhang-68423966/' }
     ]
 };
--- src/components/HomePage/data.ts
+++ src/components/HomePage/data.ts
@@ -1,19 +1,21 @@
 const data = {
     text: [
         `Hi, I'm Kia Farhang, a full-stack developer at the Express-News in San Antonio, Texas.`,
-        `This website showcases my work and a little bit about myself.`,
-        `You can navigate using the links below - or, if you'd like, use the command line at the bottom of the screen.`,
+        `You can navigate this site using the links below - or, if you'd like, use the command line at the bottom of the screen.`,
         `Type the name of the page you'd like to visit. 
         To access the "about" page, type "about" and press enter.`,
-        `"../" will take you up one level.`
+        `"../" will take you up one level.`,
+        `If you'd rather just see my resume, click the 'resume' link below.`
     ],
     links: [
         { text: 'about', href: 'about' },
-        { text: 'portfolio', href: 'portfolio' }
+        { text: 'portfolio', href: 'portfolio' },
+        { text: 'resume', href: 'https://github.com/KiaFarhang/ts-portfolio/blob/master/kia-farhang-resume.pdf' }
     ],
     commands: [
         { command: 'about', url: 'about' },
-        { command: 'portfolio', url: 'portfolio' }
+        { command: 'portfolio', url: 'portfolio' },
+        { command: 'resume', url: 'https://github.com/KiaFarhang/ts-portfolio/blob/master/kia-farhang-resume.pdf' }
     ]
 };
 
