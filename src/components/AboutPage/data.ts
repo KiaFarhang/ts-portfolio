85a888f58e2bdcea69ea4463cd5ab9f62387a7b1
Kia Farhang <kfarhang0@gmail.com>
2018-03-19 18:46:10 -0500

    Update about page to note CS degree started

--- src/components/AboutPage/data.ts
+++ src/components/AboutPage/data.ts
@@ -3,7 +3,7 @@ const data = {
         `I grew up in Minnesota, where I studied journalism in college.`,
         `That took me to Southern California, where I started coding and eventually switched careers.`,
         `I prefer back-end, test-driven development whenever possible.
-        In spring 2018, I'll begin the part-time pursuit of a computer science degree.`,
+        In spring 2018, I began the part-time pursuit of a computer science degree.`,
         `Outside of work, I enjoy playing video games, reading and taking walks with my girlfriend and dog.`
     ],
     links: [
