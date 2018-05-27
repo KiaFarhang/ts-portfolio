87ce8c191e2541985ec6c0dff8d197c39efce524
Kia Farhang <kfarhang0@gmail.com>
2018-05-27 17:27:35 -0500

    Update home page to note new job; add Distillr Java

--- src/components/HomePage/data.ts
+++ src/components/HomePage/data.ts
@@ -1,6 +1,6 @@
 const data = {
     text: [
-        `Hi, I'm Kia Farhang, a full-stack developer at the Express-News in San Antonio, Texas.`,
+        `Hi, I'm Kia Farhang, a software developer at USAA in San Antonio, Texas.`,
         `You can navigate this site using the links below - or, if you'd like, use the command line at the bottom of the screen.`,
         `Type the name of the page you'd like to visit. 
         To access the "about" page, type "about" and press enter.`,
