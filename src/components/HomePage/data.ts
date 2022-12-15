efd749490b3885d30ea0fc64a3090423bcaec923
Kia Farhang <kfarhang0@gmail.com>
2022-12-15 11:32:59 -0600

    chore: update place of work in home page

--- src/components/HomePage/data.ts
+++ src/components/HomePage/data.ts
@@ -1,12 +1,12 @@
 const data = {
     text: [
-        `Hi, I'm Kia Farhang, a software developer in Austin, Texas currently working for DataStax.`,
+        `Hi, I'm Kia Farhang, a software developer in Austin, Texas currently working for The New York Times.`,
         `You can navigate this site using the links below - or, if you'd like, use the command line at the bottom of the screen.`,
         `Type the name of the page you'd like to visit. 
         To access the "about" page, type "about" and press enter.`,
         `"../" will take you up one level.`,
         `If you'd rather just see my resume, click the 'resume' link below.`
-    ],
+     ],
     links: [
         { text: 'about', href: 'about' },
         { text: 'portfolio', href: 'portfolio' },
