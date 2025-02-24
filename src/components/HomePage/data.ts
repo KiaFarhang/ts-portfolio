434a8c9266c6032789e4bef1b71403a596a8a316
Kia Farhang <kfarhang0@gmail.com>
2025-02-24 10:43:00 -0600

    feature: restore link to resume; tweak home page text

--- src/components/HomePage/data.ts
+++ src/components/HomePage/data.ts
@@ -1,16 +1,19 @@
 const data = {
     text: [
-        `Hi, I'm Kia Farhang.`,
+        `Hi, I'm Kia Farhang, a software developer, musician and writer.`,
         `You can navigate this site using the links below - or, if you'd like, use the command line at the bottom of the screen.`,
         `Type the name of the page you'd like to visit. 
-        To access the "portfolio" page, type "portfolio" and press enter.`,
+        E.g. to access the "portfolio" page, type "portfolio" and press enter.`,
         `"../" will take you up one level.`,
+        `"resume" will take you directly to my resume, which is hosted on GitHub.`
      ],
     links: [
         { text: 'portfolio', href: 'portfolio' },
+        { text: 'resume', href: 'https://github.com/KiaFarhang/ts-portfolio/blob/master/kia-farhang-resume.pdf'}
     ],
     commands: [
         { command: 'portfolio', url: 'portfolio' },
+        { command: 'resume', url: 'https://github.com/KiaFarhang/ts-portfolio/blob/master/kia-farhang-resume.pdf'}
     ]
 };
 
